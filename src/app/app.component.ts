01b9a880ffcf41161a4c320b245ce177217be6a4
Daniel Godoy <danielgodoy66@gmail.com>
2024-08-17 13:02:19 +0200

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'listaTareasApp';
+export class AppComponent implements OnInit {
+
+  listaTareas: string[] = [];
+  nuevaTarea: string = '';
+
+  // inyeccion del servicio
+  private _tareasService = inject(TareasService);
+
+  // obtener tareas de localStorage
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  // boton de agregar tarea
+  agregarTarea() {
+    this._tareasService.agregarTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  // boton de eliminar tarea
+  eliminarTarea(index: number) {
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
 }
